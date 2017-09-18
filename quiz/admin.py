cf6a1070d299a6592ca832d3b2cbe06e07826c84
stratopedarx <stratopedarx.lobanov@yandex.ru>
2017-09-18 22:40:16 +0300

    Update admin rights.

--- quiz/admin.py
+++ quiz/admin.py
@@ -3,6 +3,24 @@ from django.contrib import admin
 from .models import Question, Option, TypeTest
 
 
-admin.site.register(Question)
+class OptionInline(admin.StackedInline):
+    model = Option
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [OptionInline]
+
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 5
+
+
+class TypeTestAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+
+
 admin.site.register(Option)
-admin.site.register(TypeTest)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(TypeTest, TypeTestAdmin)
--- quiz/models.py
+++ quiz/models.py
@@ -15,7 +15,7 @@ class TypeTest(models.Model):
 class Question(models.Model):
     """Model for questions."""
     question_text = models.CharField(max_length=400)
-    type_test = models.ForeignKey(TypeTest, on_delete=models.CASCADE)  # the communication is many-to-one
+    type_test = models.ForeignKey(TypeTest, on_delete=models.CASCADE)  # many-to-one
 
     def __str__(self):
         return self.question_text
@@ -25,10 +25,10 @@ class Option(models.Model):
     """Model for test options."""
     option_text = models.CharField(max_length=200)
     truth = models.BooleanField()  # can be False if option is not right or True if is truth
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)  # the communication is many-to-one
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)  # many-to-one
 
     def __str__(self):
-        return 'Option is {} - {}'.format(self.option, self.truth)
+        return 'Option is {} - {}'.format(self.option_text, self.truth)
 
 
 class Result(models.Model):
